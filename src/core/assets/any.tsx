18afb2273f1873c2ab751f62478985ca739c6a24
Mirafzal1307 <usmonjonovmirafzal0713@gmail.com>
2022-08-12 10:38:50 +0500

    folders are created

---
+++ src/core/assets/any.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function any(props: any) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default any;
--- src/core/pages/NotFound.tsx
+++ src/core/pages/NotFound.tsx
@@ -1,29 +1,13 @@
 import Button from "@material-ui/core/Button";
 import { useTranslation } from "react-i18next";
 import { Link as RouterLink } from "react-router-dom";
-import Result from "../../core/components/Result";
-import { ReactComponent as NotFoundSvg } from "../assets/404.svg";
 
 const NotFound = () => {
   const { t } = useTranslation();
 
   return (
-    <Result
-      extra={
-        <Button
-          color="secondary"
-          component={RouterLink}
-          to={`/${process.env.PUBLIC_URL}/admin`}
-          variant="contained"
-        >
-          {t("common.backHome")}
-        </Button>
-      }
-      image={<NotFoundSvg />}
-      maxWidth="sm"
-      subTitle={t("common.errors.notFound.subTitle")}
-      title={t("common.errors.notFound.title")}
-    />
+   <>
+   </>
   );
 };
 
