785ded77f5f4601c5b03b8b4b6db61cea29727ec
Diyorceek <jumanazarovdiyorbek2007@gmail.com>
2022-09-08 19:09:13 +0500

    fix

--- src/core/components/Category/CategoryCreateList.tsx
+++ src/core/components/Category/CategoryCreateList.tsx
@@ -1,27 +1,23 @@
 import {
   Button,
-  FormLabel,
   Grid,
   Paper,
   TextField,
-  Toolbar,
   Typography
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import DeleteIcon from "@mui/icons-material/Delete";
 import {
   cancelButton,
   note,
   noteAll,
-  saveButton,
   smartphoneSmall,
   subCategory,
   successSaveButton,
-  uploadImageCategory
 } from "../../theme/CategoryStyle";
 import Modal from "../Modal";
+import UploadModal from "../UploadModal";
 
 const useStyles = makeStyles({
   categoryInput: {
@@ -30,55 +26,22 @@ const useStyles = makeStyles({
     fontSize: "14px",
     fontWeight: "400",
     padding: "2px 0 2px 0 !important",
-    width: "320px"
-  },
-  forImagePreview: {
-    width: "95px",
-    height: "95px",
-    border: "none !important"
-  },
-  bigFatherPosition: {
-    position: "relative"
-  },
-  ClickedImage: {
-    "&:hover": {
-      width: "100%",
-      height: "100%",
-      background: "gray",
-      opacity: 1
-    }
-  },
-  positionImage: {
-    position: "absolute",
-    right: "32%",
-    top: "18%"
+    width: "320px",
   }
 });
 
 const CategoryCreateList = () => {
   const { t } = useTranslation();
-  const classes = useStyles();
-  const fileInputRef = useRef<any>();
-  const [preview, setPreview] = useState<any>();
-
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
   const [selectedImages, setSelectedImages] = useState<any>([]);
 
-  const onSelectedFile = (event: any) => {
-    console.log(event.target.files.length);
-    const selectedFiles = event.target.files;
-    const selectedFilesArray: string[] = Array.from(selectedFiles);
-
-    const imagesArray: string[] = selectedFilesArray.map((file: any) => {
-      return URL.createObjectURL(file)
-    });
-    setSelectedImages(imagesArray);
-  };
-
-  const deleteFile = (id: any) => {
-    selectedImages.filter((user: any) => user.id !== id);
-    setSelectedImages(selectedImages);
-    console.log(selectedImages);
-  };
+  // const deleteFile = (id: any) => {
+  //   selectedImages.filter((user: any) => user.id !== id);
+  //   setSelectedImages(selectedImages);
+  //   console.log(selectedImages);
+  // };
 
   return (
     <>
@@ -102,62 +65,13 @@ const CategoryCreateList = () => {
                 "admin.category_page.category_create_list.small_categories_photos"
               )}
             </Typography>
-            <Toolbar style={{ justifyContent: "space-between", padding: "0" }}>
-              <Grid sx={{ display: "flex" }}>
-                {selectedImages.map((image: any, id: any) => {
-                  return (
-                    <div
-                      key={id}
-                      style={{ paddingRight: "5px" }}
-                      className={classes.bigFatherPosition}
-                    >
-                      <img
-                        style={uploadImageCategory}
-                        className={classes.ClickedImage}
-                        src={image}
-                        alt=""
-                      />
-                      <Grid
-                        className={classes.positionImage}
-                        onClick={() => deleteFile(id)}
-                      >
-                        <DeleteIcon style={{ color: "black" }} />
-                      </Grid>
-                    </div>
-                  );
-                })}
-              </Grid>
-              <Grid sx={{ display: "flex" }}>
-                <form style={{ display: "flex", alignItems: "center" }}>
-                  <img
-                    src={preview}
-                    style={{ display: preview ? "block" : "none" }}
-                    className={classes.forImagePreview}
-                    alt="img"
-                  />
-                  <FormLabel
-                    htmlFor="file-input"
-                    onClick={(event: any) => {
-                      event.preventDefault();
-                      fileInputRef.current.click();
-                    }}
-                  >
-                    <img src={require("../../../Img/save.png")} alt="dddd" />
-                  </FormLabel>
-                  <input
-                    type="file"
-                    multiple
-                    style={{ display: "none" }}
-                    ref={fileInputRef}
-                    accept="image/*"
-                    onChange={onSelectedFile}
-                  />
-                </form>
-                {selectedImages == false ? null : <button style={saveButton}>
+              <Grid sx={{ textAlign: "end" }}>
+                <UploadModal />
+                {/* {selectedImages == false ? null : <button style={saveButton}>
                   Save
-                </button>}
+                </button>} */}
+
               </Grid>
-            </Toolbar>
             <Typography style={note}>
               {t("admin.category_page.category_create_list.note_about")}
             </Typography>
