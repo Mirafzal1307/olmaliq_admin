9a31dbec66c87763b27c26491f9ce19d5aae827d
shohjahon23 <tursunboyevshohjaxon9@gmail.com>
2022-09-10 16:30:52 +0500

    fix

--- src/core/components/Product/ProductCreate.tsx
+++ src/core/components/Product/ProductCreate.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import MiniDrawer from '../../../layouts/Drawer/Drawer'
+import { motion } from "framer-motion";
 
 const ProductCreate = () => {
   return (
-    <>
-        <MiniDrawer />
-    </>
-  )
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <MiniDrawer />
+    </motion.div>
+  );
 }
 
 export default ProductCreate
--- src/core/components/Settings.tsx
+++ src/core/components/Settings.tsx
@@ -2,17 +2,22 @@ import React from "react";
 import { Container, Typography } from "@mui/material";
 import MiniDrawer from "../../layouts/Drawer/Drawer";
 import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
 
 const Settings = () => {
   const { t } = useTranslation();
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <MiniDrawer />
       <Container>
         <Typography variant="h1">Settings page</Typography>
         {t("admin.drawer.menu_title.settings")}
       </Container>
-    </>
+    </motion.div>
   );
 };
 
