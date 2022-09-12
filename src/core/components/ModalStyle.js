3ec29e0fb4e1a9486b9411822d6bf2c16dd4592d
Diyorceek <jumanazarovdiyorbek2007@gmail.com>
2022-09-12 10:14:06 +0500

    fix

---
+++ src/core/components/ModalStyle.js
@@ -0,0 +1,16 @@
+export const modalPageStyle = {
+    h1: {
+        fontStyle: "normal",
+        fontWeight: "600",
+        fontSize: "22px",
+        color: "#2DA55F",
+        margin: "0",
+    },
+    h2: {
+        fontFamily: "Poppins",
+        fontSize: "17px",
+        fontWeight: "400",
+        margin: "10px 0 20px 0",
+        lineHeight: "138%",
+    },
+}
